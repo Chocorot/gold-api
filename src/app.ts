53d33662150bb4c1975695e000b9b404f7af1b52
Chocorot <fww@chocorot.net>
2026-03-26 23:47:53 +0800

    feat: add redirect for root path to GitHub repository

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (_req, res) => {
+	res.redirect('https://github.com/Chocorot/gold-api');
+});
+
 // All API routes are namespaced under /api
 app.use('/api', router);
 
